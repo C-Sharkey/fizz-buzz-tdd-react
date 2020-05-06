d147704f625d0d5265bb958da27bc188b00381de
C Sharkey <colinsharkey88@hotmail.com>
2020-05-06 08:22:07 +0100

    Initial Setup
    - Remove unneccissary files
    - Cleared App.js
    - Default 1st test to check header

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,17 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// Check for header
+test('renders header: "FizzBuzz TDD" ', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const headerElement = getByText(/FizzBuzz TDD/i);
+  expect(headerElement).toBeInTheDocument();
 });
+
+// render a list of numbers 1 to 100
+
+// divisible by 3 shoud render 'Fizz'
+
+// divisible by 5 should render 'buzz'
+
+// divisible by both should render 'FizzBuzz'
